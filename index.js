1ef9015e33a32650b6f3d3570baed3ba3a231f24
mianzainch000 <mianzainch000@gmail.com>
2024-12-31 12:26:56 +0500

    serverless-http

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-require("./db/config");
 require("dotenv").config();
 const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const userRoutes = require("./routes/user");
+
 const app = express();
 app.use(express.json());
-const cors = require("cors");
 app.use(cors());
-app.use((req, res, next) => {
-    res.status(404).json({ error: "Route not found" });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Internal Server Error" });
-});
-
-const userRoutes = require("./routes/user");
 
 app.use("/", userRoutes);
 
-const PORT = process.env.PORT;
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 3000; // Fallback for local development
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
